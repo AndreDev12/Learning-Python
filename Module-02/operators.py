2b17d88b5b2e3ab0da1a3bf9c56d1406a9382386
André Torres <andretorresjava@gmail.com>
2023-10-11 14:34:38 -0500

    Working with outputs.

---
+++ Module-02/operators.py
@@ -0,0 +1,3 @@
+left_side = 10
+right_side = 5
+print(type(left_side/right_side)) #<class 'float'>
---
+++ Module-02/program.py
@@ -0,0 +1,11 @@
+sum = 1 + 2
+print(sum)
+
+print('show this in the console')
+
+sum = 1 + 2
+product = sum * 2
+print(product)
+
+distance_to_alpha_centauri = 4.367
+print(type(distance_to_alpha_centauri))
