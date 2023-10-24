e5cbd27a38c4b96f5d93ea123b0e26b5b096f99f
André Torres <andretorresjava@gmail.com>
2023-10-23 21:46:52 -0500

    About the use of quotes.

---
+++ Module-05/about-use-quotes.py
@@ -0,0 +1,4 @@
+moon_radius = "The Moon has a radius of 1,080 miles."
+
+fact = """We only see about 60% of the Moon's surface, this is known as the "near side"."""
+print(fact)
--- Module-05/inmutability-chains.py
+++ Module-05/inmutability-chains.py
@@ -1,2 +1,7 @@
 fact = "The Moon has no atmosphere. "
 fact + "No sound can be heard on the Moon."
+print(fact)
+
+fact = "The Moon has no atmosphere. "
+two_facts = fact + "No sound can be heard on the Moon."
+print(two_facts)
