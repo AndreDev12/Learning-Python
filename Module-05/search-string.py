e30f44821e461caa393150144907bc73fc8df7e3
André Torres <andretorresjava@gmail.com>
2023-10-29 14:09:51 -0500

    String methods in Python.

---
+++ Module-05/search-string.py
@@ -0,0 +1,14 @@
+print("Moon" in "This text will describe facts and challenges with space travel")
+
+print("Moon" in "This text will describe facts about the Moon")
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.find("Moon"))
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.find("Mars"))
+
+# Otra manera de buscar contenido
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.count("Mars"))
+print(temperatures.count("Moon"))
