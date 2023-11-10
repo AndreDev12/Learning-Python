13a2d7068d99470664c35711f869ef12d1c23736
André Torres <andretorresjava@gmail.com>
2023-11-10 14:26:40 -0500

    Uso de operadores aritméticos y números en Python.

---
+++ Module-06/operators.py
@@ -0,0 +1,29 @@
+# Suma
+answer = 30 + 12
+print(answer) 
+
+# Resta
+difference = 30 - 12
+print(difference)
+
+# Multiplicación
+product = 30 * 12
+print(product)
+
+# División
+quotient = 30 / 12
+print(quotient)
+
+# Uso de la división
+seconds = 1042
+display_minutes = 1042 // 60
+display_seconds = 1042 % 60
+print(display_minutes)
+print(display_seconds)
+
+# Orden de las operaciones
+result_1 = 1032 + 26 * 2
+print(result_1)
+
+result_2 = 1032 + (26 * 2)
+print(result_2)
---
+++ Module-06/use-numbers.py
@@ -0,0 +1,22 @@
+# Conversión de cadenas en números
+demo_int = int('215')
+print(demo_int)
+
+demo_float = float('215.3')
+print(demo_float)
+
+# Valores absolutos
+print(abs(39 - 16))
+print(abs(16 - 39))
+
+# Redondeo
+print(round(14.5))
+
+# Biblioteca matemática
+from math import ceil, floor
+
+round_up = ceil(12.5)
+print(round_up)
+
+round_down = floor(12.5)
+print(round_down)
