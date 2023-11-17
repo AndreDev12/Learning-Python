c68ea57df5e808e40418c7dec03c834eddda8e22
André Torres <andretorresjava@gmail.com>
2023-11-17 16:33:03 -0500

    Segmentación y combinación de listas.

---
+++ Module-07/list-combination.py
@@ -0,0 +1,5 @@
+amalthea_moons = ['Metis', 'Adrastea', 'Amalthea', 'Thebe']
+galilean_moons = ['Io', 'Europa', 'Ganymede', 'Callisto']
+
+regular_satellite_moons = amalthea_moons + galilean_moons
+print('The regular satellite moons of Jupiter are', regular_satellite_moons)
---
+++ Module-07/list-segmentation.py
@@ -0,0 +1,6 @@
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+planets_before_earth = planets[0:2]
+print(planets_before_earth) # ['Mercury', 'Venus']
+
+planets_after_earth = planets[3:]
+print(planets_after_earth) # ['Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
