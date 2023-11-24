ee95f9e55921c1b9d6be1fe90188abc43a974f7b
André Torres <andretorresjava@gmail.com>
2023-11-24 13:06:55 -0500

    Ejemplo de while

--- Module-08/while-example.py
+++ Module-08/while-example.py
@@ -1,4 +1,11 @@
+# Create the variable for user input
 user_input = ''
+# Create the list to store the values
+user_list = []
 
 while user_input.lower().strip() != 'done':
+    if user_input:
+        user_list.append(user_input)
     user_input = input('Enter a new value, or done when done: ')
+
+print(user_list)
