8e789dc873e1fc18e480a9afce3f184dd8372617
André Torres <andretorresjava@gmail.com>
2023-12-04 14:12:16 -0500

    Ejercicio: Creación de diccionarios de Python.

--- Module-09/dictionary.py
+++ Module-09/dictionary.py
@@ -3,5 +3,28 @@ planet = {
     'moons': 1
 }
 
+# Lectura de valores de diccionario
 print(planet.get('name'))
 print(planet['name'])
+
+# Modificación de valores de diccionario
+# planet.update({'name': 'Makemake'})
+# planet['name'] = 'Makemake'
+# Uso de la actualización
+planet.update({'name': 'Jupiter','moons': 79})
+# Uso de corchetes
+planet['name'] = 'Jupiter'
+planet['moons'] = 79
+print(planet)
+
+# Adición y eliminación de claves
+planet['orbital period'] = 4333
+planet.pop('orbital period')
+print(planet)
+
+# Tipos de data complejos
+planet['diameter (km)'] = {
+    'polar': 133709,
+    'equatorial': 142984
+}
+print(f'{planet["name"]} polar diameter: {planet["diameter (km)"]["polar"]}')
