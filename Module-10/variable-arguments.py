1a6c9b64503eed538f1f320109ea3af82d7709e4
André Torres <andretorresjava@gmail.com>
2024-01-06 19:12:15 -0500

    Argumentos de palabra clave variable.

--- Module-10/variable-arguments.py
+++ Module-10/variable-arguments.py
@@ -1,10 +1,11 @@
-def variable_length(*args):
-    print(args)
+# Argumentos de variable
+# def variable_length(*args):
+#     print(args)
 
-print(variable_length())
-print(variable_length('one', 'two'))
-print(variable_length(None))
-print(variable_length(True, 70, 'pro'))
+# print(variable_length())
+# print(variable_length('one', 'two'))
+# print(variable_length(None))
+# print(variable_length(True, 70, 'pro'))
 
 #########################################
 def sequence_time(*args):
@@ -16,3 +17,17 @@ def sequence_time(*args):
 
 print(sequence_time(4, 14, 18))
 print(sequence_time(4, 14, 48))
+
+# Argumentos de palabra clave variable
+def variable_length(**kwargs):
+    print(kwargs)
+
+print(variable_length(tanks=1, day='Wednesday', pilots=3))
+
+def crew_members(**kwargs):
+    print(f"{len(kwargs)} astronauts assigned for this mission:")
+    for title, name in kwargs.items():
+        print(f"{title}: {name}")
+
+print(crew_members(captain='Neil Armstrong', pilot='Buzz Aldrin', command_pilot='Michael Collins'))
+# print(crew_members(captain='Neil Armstrong', pilot='Buzz Aldrin', pilot='Michael Collins')) # argumento de palabra clave repetida produce un error
